94591b781ba3541540321b860efbd93733f999cb
AbdelmajidBen <abdelmajid.benjelloun@etu.uae.ac.ma>
2024-12-28 22:02:49 +0100

    ading learning stuff

---
+++ learning/contour.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Charger une image en niveaux de gris
+image = cv2.imread('learning/Images/im3.jpeg', cv2.IMREAD_GRAYSCALE)
+
+# Étape 1 : Réduction du bruit
+blurred = cv2.GaussianBlur(image, (5, 5), 1.4)
+
+# Étape 2 : Détection automatique des seuils
+median_intensity = np.median(blurred)  # Calcul de l'intensité médiane
+
+# Fixer les seuils automatiquement en fonction de l'intensité médiane
+low_threshold = int(max(0, 0.66 * median_intensity))
+high_threshold = int(min(255, 1.33 * median_intensity))
+
+# Étape 3 : Appliquer l'algorithme de Canny
+edges = cv2.Canny(blurred, low_threshold, high_threshold)
+
+# Visualisation des résultats
+plt.figure(figsize=(10, 6))
+plt.subplot(1, 2, 1)
+plt.title('Image originale')
+plt.imshow(image, cmap='gray')
+
+plt.subplot(1, 2, 2)
+plt.title(f'Contours détectés (Canny) \nSeuils: [{low_threshold}, {high_threshold}]')
+plt.imshow(edges, cmap='gray')
+
+plt.tight_layout()
+plt.show()
---
+++ learning/filtre_guass.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+image_path = "learning/Images/im2.jpg"
+image = cv2.imread(image_path)
+if image is None:
+    print(f"Erreur : Impossible de charger l'image {image_path}. Vérifiez le chemin.")
+    exit()
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+gaussian_filtered = cv2.GaussianBlur(gray_image, (9, 9), 0)
+
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(gray_image, cmap='gray')
+plt.title("Image Originale (bruyante)")
+plt.axis("off") 
+plt.subplot(1, 2, 2)
+plt.imshow(gaussian_filtered, cmap='gray')
+plt.title("Image Filtrée (Gaussienne)")
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
